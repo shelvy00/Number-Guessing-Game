cf1b7863daf857a2c80a3508e0f7984761437a25
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2022-01-11 19:59:31 -0600

    update.

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ const output = document.getElementById("outputtext");
 
 const number = Math.floor(Math.random() * 100)
 
+let count = 0;
+const numOfGuess = 3
+
+
 function myFunction(e) {
 	//btn.addEventListener("click", function(){
 		e.preventDefault();
 		const input = document.getElementById("userInput").value;
-		if (input === number) {
+		if (input === number && count < numOfGuess) {
 			document.getElementById('p1').innerHTML = ` You guessed right number was ${number}`
-		} else if (input < number) {
+		} else if (input < number && count < numOfGuess) {
 			document.getElementById('p1').innerHTML = "You guessed too low!"
-		} else if (input > number) {
+			count++
+		} else if (input > number && count < numOfGuess) {
 			document.getElementById('p1').innerHTML = "you guessed too high!"
+			count++
+		} else if (count === numOfGuess) {
+			document.getElementById('p1').innerHTML = "Too many guesses, Game Over"
+			count*0 // I made count*0 to restart the game without the user refreshing the page
 		}
 	//})
 };
